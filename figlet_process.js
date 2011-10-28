4b3a269f3a46279fef180965801044877ca95388
Noah Adams <noah.adams@gmail.com>
2011-10-27 21:38:12 -0700

    change of interface

--- figlet_process.js
+++ figlet_process.js
@@ -1,5 +1,25 @@
-#!/usr/bin/env node
+var spawn = require('child_process').spawn
 
-(function () {
-    console.log("Hello World!");
-})();
+/**
+ * Pass some text to a figlet process, set a callback to receive the output
+ * @param {String} text String to be passed through figlet
+ * @param {String} font Name of the figlet font to use
+ * @param {Function} cb Function that will be called with the figletted string
+ */
+
+function figlet(text, font, cb) {
+  
+  var f = spawn('figlet', ['-f', font]);
+
+  f.stdin.write(text);
+  
+  // Hook up the plumbing
+  f.stdout.on('data', function (data) {
+    cb(data.toString('utf8'));
+  });
+
+  // Give figlet a kick by closing stdin
+  f.stdin.end();  
+} 
+
+module.exports = figlet;
